02d5636d34c9abbe28795ab54a485fe45371bed3
gautham-8087 <agautham2024@gmail.com>
2026-04-02 14:01:15 +0530

    resolve the workflow for student admission

--- hooks.py
+++ hooks.py
@@ -247,3 +247,9 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
+# doc_events = {
+#     "Admission Application": {
+#         "on_update": "campusflow.api.create_student_on_approval"
+#     }
+# }
