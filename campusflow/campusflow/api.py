07281fe8038ac900a1899e4bd1bdc078bcb991cd
gautham-8087 <agautham2024@gmail.com>
2026-04-03 11:05:38 +0530

    Add the api for the fee payment

--- campusflow/campusflow/api.py
+++ campusflow/campusflow/api.py
@@ -12,3 +12,13 @@ def create_student_on_approval(doc, method):
             })
             student.insert(ignore_permissions=True)
 
+@frappe.whitelist()
+def get_student_fee(student):
+    payments = frappe.get_all(
+        "Fee Payment",
+        filters={"student": student},
+        fields=["amount_paid"]
+    )
+
+    total = sum([p.amount_paid for p in payments])
+    return total
