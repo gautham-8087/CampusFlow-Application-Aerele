07920f4e879d236a50f79c89f13a81f0d3640f45
gautham-8087 <agautham2024@gmail.com>
2026-04-07 11:11:47 +0530

    git issue

--- api.py
+++ api.py
@@ -44,22 +44,20 @@ def get_student_fee(student):
 @frappe.whitelist()
 def get_pending_fees():
     total_fee = frappe.db.sql("""
-        SELECT SUM(total_fee) FROM tabFee Structure
+        SELECT SUM(total_fee) FROM `tabFee Structure`
     """)[0][0] or 0
 
-    paid = frappe.db.sql("""
-        SELECT SUM(amount_paid) 
-        FROM tabFee Payment 
+    total_paid = frappe.db.sql("""
+        SELECT SUM(amount_paid) FROM `tabFee Payment`
         WHERE docstatus = 1
     """)[0][0] or 0
 
-    pending = total_fee - paid
+    pending = total_fee - total_paid
 
-    # prevent negative values
     if pending < 0:
         pending = 0
 
     return {
-        "value": pending,
-        "fieldtype": "Currency"
+        "value":pending,
+        "fieldtype":"Currency"
     }
