720218b51480c6049cbdb4c79b384073e56e90f5
gautham-8087 <agautham2024@gmail.com>
2026-04-09 11:32:06 +0530

    Modify the api.py for clear understanding

--- api.py
+++ api.py
@@ -141,3 +141,109 @@ def get_unpaid_students():
             HAVING paid = 0
         ) AS t
     """)[0][0]
+
+
+
+
+
+
+# # -------------------------------
+# # 🎓 STUDENT CREATION
+# # -------------------------------
+# def create_student_on_approval(doc, method):
+#     if doc.status != "Approved":
+#         return
+
+#     if frappe.db.exists("Student", {"student_name": doc.applicant_name}):
+#         return
+
+#     frappe.get_doc({
+#         "doctype": "Student",
+#         "student_name": doc.applicant_name,
+#         "program": doc.program,
+#         "guardian_name": doc.guardian_name,
+#         "contact_number": doc.contact_number
+#     }).insert(ignore_permissions=True)
+
+
+# # -------------------------------
+# # 📘 STUDENT DETAILS API
+# # -------------------------------
+# @frappe.whitelist(allow_guest=True)
+# def get_student_details(student):
+#     doc = frappe.get_doc("Student", student)
+
+#     return {
+#         "student_id": doc.name,
+#         "student_name": doc.student_name,
+#         "program": doc.program,
+#         "contact": doc.contact_number,
+#         "guardian": doc.guardian_name,
+#         "total_fee": doc.total_fee
+#     }
+
+
+# # -------------------------------
+# # 📊 COMMON HELPER FUNCTIONS
+# # -------------------------------
+# def get_total_fee():
+#     return frappe.db.sql("""
+#         SELECT IFNULL(SUM(total_fee), 0) FROM `tabStudent`
+#     """)[0][0]
+
+
+# def get_total_collected_amount():
+#     return frappe.db.sql("""
+#         SELECT IFNULL(SUM(amount_paid), 0)
+#         FROM `tabFee Payment`
+#         WHERE docstatus = 1
+#     """)[0][0]
+
+
+# def get_payment_summary():
+#     return frappe.db.sql("""
+#         SELECT 
+#             s.name,
+#             IFNULL(SUM(f.amount_paid), 0) AS paid,
+#             MAX(s.total_fee) AS total_fee
+#         FROM `tabStudent` s
+#         LEFT JOIN `tabFee Payment` f 
+#             ON s.name = f.student AND f.docstatus = 1
+#         GROUP BY s.name
+#     """, as_dict=True)
+
+
+# # -------------------------------
+# # 📊 DASHBOARD APIs
+# # -------------------------------
+# @frappe.whitelist(allow_guest=True)
+# def get_total_students():
+#     return frappe.db.count("Student")
+
+
+# @frappe.whitelist(allow_guest=True)
+# def get_total_collected():
+#     return get_total_collected_amount()
+
+
+# @frappe.whitelist(allow_guest=True)
+# def get_pending_fees():
+#     return get_total_fee() - get_total_collected_amount()
+
+
+# @frappe.whitelist(allow_guest=True)
+# def get_fully_paid_students():
+#     data = get_payment_summary()
+#     return sum(1 for d in data if d.paid >= d.total_fee)
+
+
+# @frappe.whitelist(allow_guest=True)
+# def get_partial_students():
+#     data = get_payment_summary()
+#     return sum(1 for d in data if 0 < d.paid < d.total_fee)
+
+
+# @frappe.whitelist(allow_guest=True)
+# def get_unpaid_students():
+#     data = get_payment_summary()
+#     return sum(1 for d in data if d.paid == 0)
