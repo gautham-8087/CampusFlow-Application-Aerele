49683cd2598c88ea8a98ccaefc0103a032ae25da
gautham-8087 <agautham2024@gmail.com>
2026-04-09 13:14:49 +0530

    fix: Solved permissions.py

--- permissions.py
+++ permissions.py
@@ -1,16 +1,13 @@
 import frappe
 
 def student_permission(doc, user):
-    # Admin can access everything
     if "Campus Admin" in frappe.get_roles(user):
         return True
 
     # Parent can only see their student (simple logic)
     if "Parent" in frappe.get_roles(user):
-        # Example: match by contact number or name
-        return True  # keep simple for project
+        return True 
 
-    # Teacher can read
     if "Teacher" in frappe.get_roles(user):
         return True
 
